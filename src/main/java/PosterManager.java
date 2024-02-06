96e7c519ef53852130ce1acab5e2af932ff7c2a7
vexed <kadirthekid@gmail.com>
2024-02-06 23:17:13 +0300

    changed

--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -38,6 +38,7 @@ public class PosterManager {
         String[] tmp = new String[resultLength];
         for (int i = 0; i < tmp.length; i++) {
             tmp[i] = movies[movies.length - 1 - i];
+
         }
         return tmp;
     }
