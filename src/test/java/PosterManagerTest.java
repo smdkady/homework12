5e1601cd2cee26fb190385e2f858be523520a887
vexed <kadirthekid@gmail.com>
2024-02-06 21:39:59 +0300

    initial project

--- src/test/java/PosterManagerTest.java
+++ src/test/java/PosterManagerTest.java
@@ -48,6 +48,34 @@ class PosterManagerTest {
     }
 
     @Test
+    public void testFindLastLower() {
+        PosterManager manager = new PosterManager();
+        manager.add("Movie1");
+        manager.add("Movie2");
+        manager.add("Movie3");
+        manager.add("Movie4");
+        String[] actual = manager.findLast();
+        String[] expected = {"Movie4", "Movie3", "Movie2", "Movie1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLastHigher() {
+        PosterManager manager = new PosterManager(7);
+        manager.add("Movie1");
+        manager.add("Movie2");
+        manager.add("Movie3");
+        manager.add("Movie4");
+        manager.add("Movie5");
+        manager.add("Movie6");
+        manager.add("Movie7");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Movie7", "Movie6", "Movie5", "Movie4", "Movie3", "Movie2", "Movie1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void testWithLimit() {
         PosterManager manager = new PosterManager(10);
         String[] actual = manager.findAll();
